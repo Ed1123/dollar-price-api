9c2865dd801d51512610dd19cf774cd2222a9150
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-09-12 18:58:02 -0500

    Working code

--- scrape_dollar/dollar.py
+++ scrape_dollar/dollar.py
@@ -17,24 +17,22 @@ def get_html() -> str:
     return requests.get(url).text
 
 
-def get_selector(html: str) -> Selector:
-    return Selector(html)
-
-
-def parse(selector: Selector) -> list[Exchange]:
-    casas_de_cambio = selector.xpath(
+def parse(html: str) -> list[Exchange]:
+    '''Parse the html and get a list of Exchanges'''
+    exchanges = Selector(html).xpath(
         '/html/body/div[3]/section/div[1]/div[3]/div[1]/div/div'
     )
-    for box in casas_de_cambio:
-        if 'header' in box.xpath('./@class').get():
-            continue
-        yield Exchange(
+    return [
+        Exchange(
             box.xpath('.//h3/a/text()').get(),
             box.xpath('.//h3/a/@href').get(),
             box.xpath('./div[1]/div[2]/text()').get().strip(),
             box.xpath('./div[1]/div[3]/text()').get().strip(),
         )
+        for box in exchanges
+        if 'header' not in box.xpath('./@class').get()
+    ]
 
 
 if __name__ == '__main__':
-    print(get_html())
+    print(parse(get_html()))
