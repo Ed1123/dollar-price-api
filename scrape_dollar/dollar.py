293fc3006bda489bb4b7b05a19681ea408c5ff8b
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-09-12 19:06:50 -0500

    Refactor code
    Break each part of the scraping into a method in a Parser class

--- scrape_dollar/dollar.py
+++ scrape_dollar/dollar.py
@@ -17,22 +17,49 @@ def get_html() -> str:
     return requests.get(url).text
 
 
-def parse(html: str) -> list[Exchange]:
-    '''Parse the html and get a list of Exchanges'''
-    exchanges = Selector(html).xpath(
-        '/html/body/div[3]/section/div[1]/div[3]/div[1]/div/div'
-    )
-    return [
-        Exchange(
-            box.xpath('.//h3/a/text()').get(),
-            box.xpath('.//h3/a/@href').get(),
-            box.xpath('./div[1]/div[2]/text()').get().strip(),
-            box.xpath('./div[1]/div[3]/text()').get().strip(),
+class Parser:
+    def __init__(self, html: str) -> None:
+        self.selector = Selector(html)
+
+    def parse(self) -> list[Exchange]:
+        '''Parse the html and get a list of Exchanges'''
+        exchanges = self.selector.xpath(
+            '/html/body/div[3]/section/div[1]/div[3]/div[1]/div/div'
         )
-        for box in exchanges
-        if 'header' not in box.xpath('./@class').get()
-    ]
+        return [
+            Exchange(
+                self.get_name(exchange_row),
+                self.get_url(exchange_row),
+                self.get_buy_price(exchange_row),
+                self.get_sell_price(exchange_row),
+            )
+            for exchange_row in exchanges
+            if 'header' not in exchange_row.xpath('./@class').get()
+        ]
+
+    @staticmethod
+    def get_name(selector: Selector) -> str:
+        return selector.xpath('.//h3/a/text()').get()  # type: ignore
+
+    @staticmethod
+    def get_url(selector: Selector) -> str:
+        return selector.xpath('.//h3/a/@href').get()  # type: ignore
+
+    @staticmethod
+    def get_price(selector: Selector, xpath: str) -> float:
+        return float(selector.xpath(xpath).get().strip())  # type: ignore
+
+    def get_buy_price(self, selector: Selector) -> float:
+        return self.get_price(selector, './div[1]/div[2]/text()')
+
+    def get_sell_price(self, selector: Selector) -> float:
+        return self.get_price(selector, './div[1]/div[3]/text()')
+
+
+def main():
+    parser = Parser(get_html())
+    print(parser.parse())
 
 
 if __name__ == '__main__':
-    print(parse(get_html()))
+    main()
