e59acdbedc973fa65f846a5eec664797677c0eb0
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-03-31 12:11:33 -0500

    Fix /rates endpoint

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ async def root():
 
 
 @app.get("/rates", response_model=list[Exchange])
-def read_exchange_rates() -> list[Exchange]:
-    return get_all_exchanges()
+async def read_exchange_rates() -> list[Exchange]:
+    return await get_all_exchanges()
