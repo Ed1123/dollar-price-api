f1b5f6d7e9264129e2ab3c35f0f5b2c2c17a51ed
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-03-31 12:11:56 -0500

    Make test async
    Add pytest-asyncio

--- tests/test_dollar.py
+++ tests/test_dollar.py
@@ -1,9 +1,12 @@
+import pytest
+
 from dollar_price.dollar import Exchange, get_all_exchanges
 
 
-def test_get_all_exchanges():
+@pytest.mark.asyncio
+async def test_get_all_exchanges():
     '''Test the main function that gets all the scrape data.'''
-    rates = get_all_exchanges()
+    rates = await get_all_exchanges()
     assert len(rates) > 0
     assert isinstance(rates[0], Exchange)
     assert rates[0].__dict__['buy_price'] > 0
